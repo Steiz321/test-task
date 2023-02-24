658c47b3200133368751ec04d1d4d8c548c8010d
Steiz321 <fundamentdaiil@gmail.com>
2023-02-24 14:46:22 +0200

    fix commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,6 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Page } from "../models/Page";
 
-const connection = new Sequelize({
-    dialect: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    models: [Page]
-})
+const connection = new Sequelize('postgres://test:JGLt5HpXKJhN3qqj9w7YWOxD84pWSu74@dpg-cfs7l81gp3jt6tj4i0h0-a.frankfurt-postgres.render.com/testtask1?ssl=true')
 
 export default connection;
