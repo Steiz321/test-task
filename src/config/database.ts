d925e9e2287edfbef8afb66e4217ba0921ce2d6e
Steiz321 <fundamentdaiil@gmail.com>
2023-02-24 12:45:37 +0200

    fix commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,15 @@
 import { Sequelize } from 'sequelize-typescript';
-import { Page } from "../models/Page";
 
-const connection = new Sequelize({
+// @ts-ignore
+const connection = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    models: [Page]
-})
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
+
 
 export default connection;
